b6c143b3266c13390604bb4e809fff47a35e1541
Taufiq Aditya Putra <taufiqadityaputra@gmail.com>
2024-01-07 20:13:53 +0700

    initial commit

--- src/components/Porto.jsx
+++ src/components/Porto.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import dataweb from '../data/frontend.json';
 import datauiux from '../data/uiux.json';
 import datadesain from '../data/desain.json';
