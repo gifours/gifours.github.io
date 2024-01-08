6ceb3084031348401215e40e9a0658f5c001534c
Taufiq Aditya Putra <taufiqadityaputra@gmail.com>
2024-01-08 15:01:55 +0700

    Initial commit

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -16,33 +16,95 @@ const MotionDiv = ({ id, title, content }) => {
   };
 
   return (
-    <div ref={ref}>
-      <motion.div
-        initial="hidden"
-        animate={inView ? 'visible' : 'hidden'}
-        variants={motionVariants[`div${id}`]}
-        transition={{ duration: 0.5 }}
-      >
-        <h1>{title}</h1>
-        <p>{content}</p>
-      </motion.div>
-    </div>
-  );
-};
+    <ul class="w-full flex flex-wrap justify-start xl:w-10/12 xl:mx-auto">
+                {/* <!-- frontend  --> */}
+                <li class="w-full item_wrap pt-12 mb-4 frontend all">
+                    <div class="max-w-full mx-auto text-center">
+                        <h4 class="porto-h4">Frond End Dev</h4>
+                        <div className="line"></div>
+                    </div>
+                </li>
+                {dataweb.map((card, i) => (
+                <div key={i} className="md:w-1/2">
+                    <li class="w-full p-4 item_wrap frontend all">
+                        <div class="bg-white rounded-xl shadow-lg dark:bg-dark p-4 dark:hover:shadow-primary2 duration-500 transition-all ease-linear group">
+                            <img className="w-full drop-shadow-sm shadow-md group-hover:scale-105 ease-linear duration-200 rounded-md" src={card.imageweb} alt="home" />
+                            <div class="justify-between">
+                                <div class="mb-2 pt-5">
+                                    <h4 class="card__title">{card.judulweb}</h4>
+                                    <p class="paragraph">{card.deskripsiweb}</p>
+                                </div>
+                                <div class="justify-between flex pt-2">
+                                    <div>
+                                        <a href={card.linkweb} class="more-btn">Lihat Detail</a>
+                                    </div>
+                                    <div class="justify-between flex">
+                                        <p class="paragraph pr-2">Make by:</p>
+                                        <svg class="logo-frontend" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d={card.imgtool1}/>
+                                        </svg>
+                                        <svg class="logo-frontend" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d={card.imgtool2}/>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </div>
+                ))}
 
-const App = () => {
-  const divs = [
-    { id: 1, title: 'Div 1', content: 'Content for Div 1' },
-    { id: 2, title: 'Div 2', content: 'Content for Div 2' },
-    { id: 3, title: 'Div 3', content: 'Content for Div 3' },
-  ];
+                {/* <!-- uiux  --> */}
+                <li class="w-full item_wrap pt-12 mb-4 uiux all">
+                    <div class="max-w-full mx-auto text-center">
+                        <h4 class="porto-h4">UI/UX Designer</h4>
+                        <div className="line"></div>
+                    </div>
+                </li>
+                {datauiux.map((card, i) => (
+                <div key={i} className="md:w-1/3">
+                    <li class="w-full item_wrap p-4 uiux all">
+                        <img src={card.imageuiux} alt="" class="px-4 md:px-0 w-full drop-shadow-lg dark:hover:drop-shadow-[0_8px_5px_#F4D160] hover:scale-110 dark:decoration-purple-200 ease-linear transition-all duration-500 rounded-md"/>
+                        <div class="mb-2 pt-5 text-center">
+                            <h4 class="card__title">{card.juduluiux}</h4>
+                            <p class="paragraph">{card.deskripsiuiux}</p>
+                        </div>
+                    </li>
+                </div>
+                ))}
 
-  return (
-    <div>
-      {divs.map((div) => (
-        <MotionDiv key={div.id} id={div.id} title={div.title} content={div.content} />
-      ))}
-    </div>
+                {/* <!-- desain  --> */}
+                <li class="w-full item_wrap pt-12 mb-4 desain all">
+                    <div class="max-w-full mx-auto text-center">
+                        <h4 class="porto-h4">Graphic Designer</h4>
+                        <div className="line"></div>
+                    </div>
+                </li>
+                {datadesain.map((card, i) => (
+                    <div key={i} className="w-1/2 md:w-1/4">
+                        <li class="item_wrap p-4 desain all">
+                            <img src={card.imagedesain} alt="" class="w-full shadow-md hover:scale-110 ease-linear duration-500 rounded-md dark:hover:shadow-primary2 dark:hover:shadow-lg"/>
+                        </li>    
+                    </div>
+                ))}
+
+                {/* <!-- blender  --> */}
+                <li class="w-full item_wrap pt-12 mb-4 blender all">
+                    <div class="max-w-full mx-auto text-center">
+                        <h4 class="porto-h4">3D Modelling</h4>
+                        <div className="line"></div>
+                    </div>
+                </li>
+                {datablender.map((card, i) => (
+                    <div key={i} className="md:w-1/3">
+                        <li class="w-full item_wrap p-4 blender all">
+                            <div class="rounded-lg shadow-md  overflow-hidden relative hover:scale-95 transition-all duration-500 dark:hover:shadow-primary2 dark:hover:shadow-lg ease-linear group">
+                                <img src={card.imageblender} alt="" class="h-full w-full group-hover:scale-150 group-hover:rotate-12 transition-all duration-300 ease-linear"/>
+                            </div>
+                        </li>  
+                    </div>
+                ))}
+            </ul>
   );
 };
 
